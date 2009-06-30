f0d48f14187560c74e0cdc3fc8ec30e6837300c3
Craig Smith <craigmarksmith@gmail.com>
2009-06-30 14:05:44 +0100

    Refactored so the code is a little DRYer.
    Also added some explaining variables

--- vigenere_cipher_test.rb
+++ vigenere_cipher_test.rb
@@ -4,7 +4,7 @@ require 'vigenere_cipher'
 class VigenereCipherTest < Test::Unit::TestCase
 
   def test_should_encrypt_a_string
-    plain_text     = 'ATTACKATDAWN';
+    plain_text = 'ATTACKATDAWN';
     expected_result = 'LXFOPVEFRNHR';
 
     machine = VigenereCipher.new;
@@ -12,5 +12,15 @@ class VigenereCipherTest < Test::Unit::TestCase
 
     assert_equal expected_result, encrypted_string
   end
-  
+
+  def test_should_decrypt_a_string
+    expected_result = 'ATTACKATDAWN';
+    encrypted_message = 'LXFOPVEFRNHR';
+
+    machine = VigenereCipher.new;
+    encrypted_message = machine.decrypt('LEMONLEMONLE',encrypted_message);
+
+    assert_equal expected_result, encrypted_message
+  end
+
 end
